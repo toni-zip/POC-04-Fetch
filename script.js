4a605c28b6f20105f969bbe095119936586edacb
Kauan Rodrigues <108241380+KauanRodriguesF@users.noreply.github.com>
2024-10-01 14:26:21 -0300

    Versão Final
    -Adicionado botão de trocar imagem

--- script.js
+++ script.js
@@ -4,14 +4,18 @@ async function getRaposa(endpoint) {
         let data = await response.json();
         let urlIGM = data.image;  
         let content = document.getElementById('imagem-container');
-        content.innerHTML += `<img src="${urlIGM}" alt="Raposa" />`;
+        content.innerHTML = `<img src="${urlIGM}" alt="Raposa" />`;
     } catch (error) {
         console.log(error);
     } finally {
-        console.log("Sua imagem foi gerado com sucesso!");
+        console.log("Sua imagem foi gerada com sucesso!");
     }
 }
 
 window.onload = () => {
-    getRaposa("https://randomfox.ca/floof/");
+    getRaposa("https://randomfox.ca/floof/"); //Para iniciar a pagina tendo um imagem
+
+    document.getElementById('trocar-imagem').addEventListener('click', function() { //Para quando clicar no botão gerar uma nova imagem
+        getRaposa("https://randomfox.ca/floof/");
+    });
 };
